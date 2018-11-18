39dffb2eb95ee71147bc86dd644660626a2bcfb5
Feilong Huang <felixh6888@gmail.com>
2018-11-17 22:41:36 -0500

    v1

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDIb8JhVzacUX5EnZi0ymddTL3df3BPZwk",
+    authDomain: "train-scheduler-c4f09.firebaseapp.com",
+    databaseURL: "https://train-scheduler-c4f09.firebaseio.com",
+    projectId: "train-scheduler-c4f09",
+    storageBucket: "train-scheduler-c4f09.appspot.com",
+    messagingSenderId: "96194083221"
+  };
+  firebase.initializeApp(config);
