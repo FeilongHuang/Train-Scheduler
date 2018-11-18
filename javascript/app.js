748b2cf5c13615a6ac46bb02f4b656767634a55a
Feilong Huang <felixh6888@gmail.com>
2018-11-18 00:46:40 -0500

    v2

--- javascript/app.js
+++ javascript/app.js
@@ -7,4 +7,20 @@ var config = {
     storageBucket: "train-scheduler-c4f09.appspot.com",
     messagingSenderId: "96194083221"
   };
-  firebase.initializeApp(config);
+  firebase.initializeApp(config); 
+  var database=firebase.database();
+  $("#submit-button").on("click",function(){
+    event.preventDefault();
+    // Get the input values
+    var TrainName=$("#train-name").val().trim();
+    var Destination=$("#destination").val().trim();
+    var FirstTrainName=$("#first-train-name").val().trim();
+    var Frequency=$("#frequecy").val().trim();
+    database.ref().push({
+    TrainName:TrainName,
+    Destination:Destination,
+    FirstTrainName:FirstTrainName,
+    Frequency:Frequency
+    });
+    $(".form-control").empty();
+})
